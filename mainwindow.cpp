8f5b1a4da252b9e79b809c9b2c8d5c979f66efa3
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-04-07 17:26:22 +0300

    Seperate layout creation into several functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,47 +17,58 @@ QWidget* setupHeader(QWidget *parent, const int fontSize) {
     return header;
 }
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
-    // Основной контейнер
-    QWidget *mainWidget = new QWidget(this);
-    QVBoxLayout *mainLayout = new QVBoxLayout(mainWidget);
-    mainLayout->setContentsMargins(10, 10, 10, 10); // Отступы от краев окна
-    setCentralWidget(mainWidget);
+QTableWidget* setupTable(QWidget *parent) {
+    // Правая часть - таблица
+    QTableWidget *table = new QTableWidget(3, 20, parent);
+    table->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    table->setStyleSheet("border: 1px solid blue;"); // Для визуализации
+    table->resizeColumnsToContents();
+    return table;
+}
 
-    // Хедер
-    QWidget *header = setupHeader(mainWidget, 20);
-    mainLayout->addWidget(header);
-
-    // Основная секция с данными
-    QWidget *dataSection = new QWidget(mainWidget);
-    dataSection->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
-    dataSection->setMaximumHeight(600);
-    dataSection->setStyleSheet("border: 1px solid green;"); // Для визуализации
-    mainLayout->addWidget(dataSection, 1); // Занимаем оставшееся пространство
-
-    // Горизонтальный layout для разделения на 2 части
-    QHBoxLayout *dataSectionLayout = new QHBoxLayout(dataSection);
-    dataSectionLayout->setContentsMargins(0, 0, 0, 0); // Убираем внутренние отступы
-
-    // Левая часть - характеристики
-    QWidget *statsPanel = new QWidget(dataSection);
+QWidget *setupStats(QWidget *parent) {
+    QWidget *statsPanel = new QWidget(parent);
     statsPanel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     statsPanel->setStyleSheet("border: 1px solid red;"); // Для визуализации
 
     QVBoxLayout *statsLayout = new QVBoxLayout(statsPanel);
     statsLayout->addWidget(new QLabel("Характеристики", statsPanel));
-    // Здесь можно добавить другие элементы характеристик
+    return statsPanel;
+}
 
-    // Правая часть - таблица
-    QTableWidget *table = new QTableWidget(1, 20, dataSection);
-    table->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    table->setStyleSheet("border: 1px solid blue;"); // Для визуализации
+QWidget *setupDataSection(QWidget *parent) {
+    QWidget *dataSection = new QWidget(parent);
+    dataSection->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
+    dataSection->setMaximumHeight(600);
+    dataSection->setStyleSheet("border: 1px solid green;"); // Для визуализации
+
+    // Горизонтальный layout для разделения на 2 части
+    QHBoxLayout *dataSectionLayout = new QHBoxLayout(dataSection);
+    dataSectionLayout->setContentsMargins(0, 0, 0, 0); // Убираем внутренние отступы
+
+    auto *table = setupTable(dataSection);
+    auto *statsPanel = setupStats(dataSection);
 
     // Распределяем пространство 1:1 между панелью и таблицей
     dataSectionLayout->addWidget(statsPanel, 1); // stretch factor = 1
     dataSectionLayout->addWidget(table, 1);      // stretch factor = 1
+    return dataSection;
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    // Основной контейнер
+    QWidget *mainWidget = new QWidget(this);
+    setCentralWidget(mainWidget);
+
+    auto *header = setupHeader(mainWidget, 20); // Хедер
+    auto *dataSection = setupDataSection(mainWidget);     // Основная секция с данными
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(mainWidget);
+    mainLayout->setContentsMargins(10, 10, 10, 10); // Отступы от краев окна
+    mainLayout->addWidget(header);
+    mainLayout->addWidget(dataSection, 1); // Занимаем оставшееся пространство
 
     this->setWindowState(Qt::WindowMaximized);
 }
