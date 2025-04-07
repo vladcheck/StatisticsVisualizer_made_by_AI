d9cd867b61e14b3c0593e4eda2afeb773573ebd6
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-04-07 07:53:12 +0300

    Setup layout and table element

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,13 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    QHBoxLayout *layout = new QHBoxLayout(centralWidget);
+
+    QTableWidget *table = new QTableWidget(1,10,centralWidget);
+    layout->addWidget(table);
 }
 
 MainWindow::~MainWindow()
